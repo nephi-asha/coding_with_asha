d027b746540e4e3fe5318dbfd5ab4a9a78db6883
Nephi Asha <37028125+nephi-asha@users.noreply.github.com>
2022-05-31 23:32:20 +0000

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!*)wn(ri#l!@(#8!$p%2@z9qd!wi@&k3rolp86hztilu&ny89v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.43.224']
+ALLOWED_HOSTS = ['']
 
 # Application definition
 
